f6ea20518ca3caafee27ba5301bc9e079972dd98
Ubuntu <ubuntu@ip-172-26-3-24.ap-south-1.compute.internal>
2023-06-30 14:42:13 +0000

    fix safe_eval

--- reportlab/reportlab-3.6.13/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-#Copyright ReportLab Europe Ltd. 2000-2017
-#see license.txt for license details
--- reportlab/reportlab-3.6.13/tools/docco/__init__.py
+++
@@ -1,3 +0,0 @@
-#Copyright ReportLab Europe Ltd. 2000-2017
-#see license.txt for license details
-#history https://hg.reportlab.com/hg-public/reportlab/log/tip/tools/docco/__init__.py
